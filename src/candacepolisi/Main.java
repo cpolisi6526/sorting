030beb394ce29d8b5c4fe9494eccf786a8246d89
BT_1N3_30@bktech.local <BT_1N3_30@bktech.local>
2019-01-07 08:38:52 -0500

    Initial commit

---
+++ src/candacepolisi/Main.java
@@ -0,0 +1,18 @@
+package candacepolisi;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Before: ");
+        int[] arr= sorting.randArr(7);
+        for (int a=0;a<arr.length;a++){
+            System.out.print(" "+ arr[a]);
+        }
+        System.out.println(" \n After: ");
+        sorting.selection(arr);
+        for (int a=0;a<arr.length;a++){
+            System.out.print(" "+ arr[a]);
+        }
+	// write your code here
+    }
+}
---
+++ src/candacepolisi/sorting.java
@@ -0,0 +1,83 @@
+package candacepolisi;
+
+public class sorting {
+
+        public static void swap(int arr[], int a, int b){
+            int temp=arr[a];
+            arr[a]=arr[b];
+            arr[b]=temp;
+        }
+
+
+
+        public static void bubbleSort(int[] arr){
+            boolean swap = true;
+            while(swap){
+                swap=false;
+                for(int i =0; i<arr.length-1; i++){
+                    if(arr[i]>arr[i+1]){
+                        swap(arr, i, i+1);
+
+                        swap = true;
+                    }
+                }
+
+            }
+        }
+
+
+        public static int[] randArr(int count)
+        {
+            int[] arr = new int[count];
+            for (int i=0; i<count; i++)
+            {
+                arr[i] = (int)(Math.random()*10);
+            }
+            return arr;
+        }
+
+        public static void checker(){
+            long time= System.nanoTime();
+            bubbleSort(randArr(4));
+            time=System.nanoTime()-time;
+            System.out.println("Time taken: " + time);
+        }
+
+        public static boolean isSorted(int[] arr){
+            for (int x:arr){
+                if (arr[x]>arr[x+1]){
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public static boolean checkSum(int[]before, int[] after){
+            int s1= 0 ;
+            for (int x:before){
+                s1+=before[x];
+            }
+            int s2=0;
+            for (int x:after){
+                s1+=after[x];
+            }
+            return (s1==s2);
+        }
+
+        public static void selection(int[] arr) {
+            boolean swap = true;
+            while (swap) {
+                swap=false;
+                for(int i=0; i<arr.length;i++){
+                    for(int x=0;x<arr.length;x++){
+                        if(arr[i]>arr[x]){
+                            swap(arr,arr[i],arr[x]);
+                        }
+                    }
+                }
+
+            }
+        }
+    }
+
+
