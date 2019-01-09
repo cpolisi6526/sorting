13e2e17cdf01eeded99bb04ffe4a65e989e57cde
BT_1N3_30 <cpolisi6526@bths.edu>
2019-01-09 08:37:42 -0500

    better fix insertion and rantintarr

--- src/candacepolisi/Runner.java
+++ src/candacepolisi/Runner.java
@@ -2,9 +2,19 @@ package candacepolisi;
 
 public class Runner {
 
+    public static void checker(double[] arr) {
+        long time = System.nanoTime();
+        sorting.selection(arr);
+        time = System.nanoTime() - time;
+        System.out.println("Time taken: " + time);
+    }
+
     public static void main(String[] args) {
         System.out.println("Before: ");
-        double[] arr= sorting.randDoubleArr(7);
+        //int[] arr= {2,5,1,7,9,3,4};
+       double arr[]= {2.1,3.2,5.2,0.1,0.2};
+      //  double[] arr= sorting.randDoubleArr(7);
+       // String arr[]= {"i","am","very","i"};
         for (int a=0;a<arr.length;a++){
             System.out.print(" "+ arr[a]);
         }
@@ -13,6 +23,9 @@ public class Runner {
         for (int a=0;a<arr.length;a++){
             System.out.print(" "+ arr[a]);
         }
+        System.out.println("\n Sorted: " + sorting.isSorted(arr));
+        double arr1[]= {2.1,3.2,5.2,0.1,0.2};
+        checker(arr1);
 	// write your code here
     }
 }
--- src/candacepolisi/sorting.java
+++ src/candacepolisi/sorting.java
@@ -26,7 +26,7 @@ public class sorting {
         while (swap) {
             swap = false;
             for (int i = 0; i < arr.length - 1; i++) {
-                if (arr[i].compareTo(arr[i + 1])>0) {
+                if (arr[i].compareTo(arr[i + 1]) > 0) {
                     swap(arr, i, i + 1);
 
                     swap = true;
@@ -53,16 +53,29 @@ public class sorting {
         return arr;
     }
 
-    public static void checker() {
-        long time = System.nanoTime();
-        //bubbleSort();
-        time = System.nanoTime() - time;
-        System.out.println("Time taken: " + time);
-    }
+
 
     public static boolean isSorted(int[] arr) {
-        for (int x = 0; x < arr.length; x++) {
+        for (int x = 0; x < arr.length-1; x++) {
             if (arr[x] > arr[x + 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean isSorted(double[] arr) {
+        for (int x = 0; x < arr.length-1; x++) {
+            if (arr[x] > arr[x + 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean isSorted(String[] arr) {
+        for (int x = 0; x < arr.length-1; x++) {
+            if (arr[x].compareTo(arr[x + 1])>=0){
                 return false;
             }
         }
@@ -94,33 +107,41 @@ public class sorting {
 
 
     public static void selection(double[] arr) {
-        int l = 0;
+        int index = 0;
         for (int i = 0; i < arr.length; i++) {
-            l = min(arr, i);
-            swap(arr, i, l);
-        }
-    }
-
-    public static void selection(int[] arr) {
-        double min;
-        int index;
-        for (int i = 0; i < arr.length; i++) {
-            min = arr[i];
-            index = i;
-            for (int j = i; j < arr.length; j++) {
-                if (arr[j] < min) {
-                    min = arr[j];
-                    index = j;
-                }
-            }
-            if (arr[i] > min) {
+            index = min(arr, i);
+            if (arr[i] > arr[index]) {
                 swap(arr, i, index);
             }
-
-
         }
+    }
 
+    public static void insertion(int[] arr) {
+        int index = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (i == 0) {
+                if (!(arr[i] < arr[i + 1])) {
+                    while ((!(arr[i] < arr[i + 1]))) {
+                        index++;
+                        if ((!((arr[index] < arr[index + 1])))) {
+                            swap(arr, i, index);
+                        }
+                    }
+                }
+            }
+            if ((i > 0) && (i<arr.length-2)) {
+                if (!((arr[i - 1] < arr[i]) && (arr[i] < arr[i + 1]))) {
+                    while ((!((arr[i - 1] < arr[i]) && (arr[i] < arr[i + 1])))) {
+                        index++;
+                        if ((!((arr[index - 1] < arr[index]) && (arr[index] < arr[index + 1])))) {
+                            swap(arr, i, index);
+                        }
+                    }
+                }
+            }
+        }
     }
 }
+
 
 
